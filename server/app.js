a695326d7566338d38011e599fa7b2bd6f6d7173
EdogawaKazuki <galad.zhao@gmail.com>
2024-10-05 03:05:53 +0800

    - updated ssl

--- server/app.js
+++ server/app.js
@@ -1,41 +1,74 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var robot = require('robotjs');
+const path = require('path');
+// const WebSocketServer = require('ws').Server;
+var expressWs = require('express-ws')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// for cross site
+var cors = require('cors');
+app.use(cors());
+// for https
+const fs = require('fs');
+const https = require('https');
+const key = fs.readFileSync('./ssl/key.pem');
+const cert = fs.readFileSync('./ssl/cert.pem');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var credentials = {key: key, cert: cert};
+var httpsServer = https.createServer(credentials, app);
+// app.get('/', (req, res) => {
+//   var indexFile = path.join(__dirname, '../public/index.html')
+//   console.log(indexFile)
+//   res.sendFile(indexFile);
+// })
+app.use(express.static('public'))
+var useHttps = true;
+if(useHttps){
+  expressWs(app, httpsServer);
+  httpsServer.listen(3001);
+} else {
+  expressWs(app);
+  app.listen(3001, function () {
+    console.log('Example app listening on port 3001!')
+  })
+}
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.ws('/socket.io', (ws, req) => {
+  console.log('socket.io connected');
+  var orientation = {alpha: 0, beta: 0, gamma: 0}
+  ws.on('message', (msg) => {
+    // console.log('message from client: ', JSON.parse(msg));
+    var data = JSON.parse(msg)
+    var vertical_range = 45
+    var horizontal_range = 45
+    var screen_width_center = robot.getScreenSize().width / 2
+    var screen_height_center = robot.getScreenSize().height / 2
+    var vertical_scale = robot.getScreenSize().height / vertical_range
+    var horizontal_scale = robot.getScreenSize().width / horizontal_range
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    if (data.type === "orientation"){
+      // console.log(data)
+      var mouse = robot.getMousePos();
+      // console.log(mouse)
+      // Move the mouse to the specified position
+      var alpha = data.data.alpha > 180 ? data.data.alpha - 360 : data.data.alpha
+      var beta = data.data.beta
+      console.log(alpha, beta)
+      // console.log("Mouse before: ", mouse.x, mouse.y)
+      robot.moveMouse(screen_width_center - horizontal_scale * (alpha - orientation.alpha), 
+                      screen_height_center - vertical_scale * (beta - orientation.beta));
+      mouse = robot.getMousePos();
+      // console.log('mouse moved:', mouse.x, mouse.y)
+    }
+    else if (data.type === "calibration"){
+      orientation = data.data
+    }else if(data.type === "leftClick"){
+      robot.mouseClick()
+    }else if(data.type === "rightClick"){
+      robot.mouseClick("right")
+    }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+    ws.send('hello from server');
+  })
+})
 module.exports = app;
